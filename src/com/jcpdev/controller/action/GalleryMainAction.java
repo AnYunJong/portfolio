5e6871871e5fc546de0aa75ffc2b60c822c0547f
Anyunjong <dksdbswhd12@naver.com>
2021-12-20 19:15:52 +0900

    회사제출용 포트폴리오

---
+++ src/com/jcpdev/controller/action/GalleryMainAction.java
@@ -0,0 +1,41 @@
+package com.jcpdev.controller.action;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.jcpdev.dao.GuguGalleryDao;
+import com.jcpdev.dao.GuguUserDao;
+import com.jcpdev.dto.GuguGallery;
+import com.jcpdev.dto.TotalProfile;
+
+public class GalleryMainAction implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		int v_idx = Integer.parseInt(request.getParameter("v_idx")); //홈의 idx
+		
+		GuguGalleryDao gdao = GuguGalleryDao.getInstance();		
+		List<GuguGallery> list = gdao.getList(v_idx); //사진정보
+		
+		List<String> category = gdao.category(v_idx);
+		
+		request.setAttribute("glist", list); 
+		request.setAttribute("categorylist", category); 
+		request.setAttribute("v_idx", v_idx);
+		GuguUserDao udao = GuguUserDao.getInstance();
+		String name = udao.getName(v_idx);
+		request.setAttribute("name", name);
+		
+		ActionForward forward = new ActionForward();
+		forward.isRedirect = false;
+		forward.url="view/gallery.jsp";
+		return forward;
+	}
+
+}
---
+++ src/com/jcpdev/controller/action/GbookCommentDeleteAction.java
@@ -0,0 +1,35 @@
+package com.jcpdev.controller.action;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.jcpdev.dao.GuestBookDao;
+import com.jcpdev.dto.GbookComment;
+
+public class GbookCommentDeleteAction implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		int idx = Integer.parseInt(request.getParameter("idx")); //댓글 자체의 idx
+        int u_idx = Integer.parseInt(request.getParameter("u_idx")); // 댓글 남긴 사람의 idx
+        int book_idx = Integer.parseInt(request.getParameter("book_idx")); // 방명록의 idx 
+
+        GbookComment dto = new GbookComment();
+        dto.setIdx(idx);
+        dto.setU_idx(u_idx);
+		
+		GuestBookDao dao = GuestBookDao.getInstance();
+        dao.CommentDelete(dto);
+		System.out.println("Gbook Comment Delete 성공");
+		
+		ActionForward forward = new ActionForward();
+		forward.isRedirect = true;
+		forward.url="gbookCommentList.do?idx="+book_idx;
+		return forward;
+	}
+
+}
